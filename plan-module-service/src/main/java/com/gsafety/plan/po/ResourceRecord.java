0b3215de700878dddaa61d7423998479b40c3129
吴清辉 <937871009@qq.com>
2016-12-18 13:52:32 +0800

    test

--- plan-module-service/src/main/java/com/gsafety/plan/po/ResourceRecord.java
+++ plan-module-service/src/main/java/com/gsafety/plan/po/ResourceRecord.java
@@ -26,6 +26,7 @@ public class ResourceRecord implements Serializable{
 	private String resourceNumber;
 	private String resourceUnit;
 
+
     private Mission missionSnR;
 
     @Id
@@ -63,7 +64,9 @@ public class ResourceRecord implements Serializable{
     }
     public void setResourceUnit(String resourceUnit) {
         this.resourceUnit = resourceUnit;
-    }	
+    }
+	
+	
 	@ManyToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name="mission_sn",referencedColumnName="mission_sn")  
     public Mission getMissionSnR() {
