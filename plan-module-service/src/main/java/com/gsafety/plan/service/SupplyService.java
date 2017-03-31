fef30f447119eee239e97b9681f8a6089c46ae2d
李珂 <809220024@qq.com>
2017-03-31 20:26:12 +0800

    map

--- plan-module-service/src/main/java/com/gsafety/plan/service/SupplyService.java
+++ plan-module-service/src/main/java/com/gsafety/plan/service/SupplyService.java
@@ -1,7 +1,10 @@
 package com.gsafety.plan.service;
 
+import net.sf.json.JSONArray;
+
 import com.gsafety.cloudframework.common.base.service.IBaseService;
 
 public interface SupplyService extends IBaseService{
 	public String getAllSupply();
+	public JSONArray getMapSupply();
 }
--- plan-module-service/src/main/java/com/gsafety/plan/service/impl/SupplyServiceImpl.java
+++ plan-module-service/src/main/java/com/gsafety/plan/service/impl/SupplyServiceImpl.java
@@ -33,12 +33,39 @@ public class SupplyServiceImpl extends BaseServiceImpl implements SupplyService{
 			 JSONObject jo = new JSONObject();
 			 jo.put("supplyName", s.getSupplyName());
 			 jo.put("supplySn", s.getSupplySn());
+			 jo.put("supplyNumber", s.getSupplyNumber());
 			 jo.put("id", s.getId());
+			 jo.put("supplyLatitude", s.getSupplyLatitude());
+			 jo.put("supplyLongitude", s.getSupplyLongitude());
+			 jo.put("supplyPrincipal", s.getSupplyPrincipal());
+			 jo.put("supplyPrincipalPhone", s.getSupplyPrincipalPhone());
 			 array.add(jo);	 
 		 }
  
 		 String str=array.toString();
 	        return str;
 	 }
-	 
+     //返回类型不一样
+	@Override
+	public JSONArray getMapSupply() {
+		 String hql="from Supply s";
+		 List<Supply> sList= (List<Supply>) baseDAO.getListByHql(hql);
+		 JSONArray array = new JSONArray();
+		 for(Supply s:sList){
+			 JSONObject jo = new JSONObject();
+			 jo.put("supplyName", s.getSupplyName());
+			 jo.put("supplySn", s.getSupplySn());
+			 jo.put("supplyNumber", s.getSupplyNumber());
+			 jo.put("id", s.getId());
+			 jo.put("supplyLatitude", s.getSupplyLatitude());
+			 jo.put("supplyLongitude", s.getSupplyLongitude());
+			 jo.put("supplyPrincipal", s.getSupplyPrincipal());
+			 jo.put("supplyPrincipalPhone", s.getSupplyPrincipalPhone());
+			 array.add(jo);	 
+		 }
+	        return array;
+		
+	}
+
+	
 }
