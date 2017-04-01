2103a5460a7bc0a0b966ab0220b7fe8fdfe9dbc9
吴清辉 <937871009@qq.com>
2017-04-01 16:56:18 +0800

    模块待完善

--- plan-module-service/src/main/java/com/gsafety/plan/service/impl/ModuleServiceImpl.java
+++ plan-module-service/src/main/java/com/gsafety/plan/service/impl/ModuleServiceImpl.java
@@ -1,8 +1,10 @@
 package com.gsafety.plan.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.gsafety.cloudframework.common.base.service.impl.BaseServiceImpl;
 import com.gsafety.plan.service.ModuleService;
-
+@Service
 public class ModuleServiceImpl extends BaseServiceImpl implements ModuleService{
 
 }
