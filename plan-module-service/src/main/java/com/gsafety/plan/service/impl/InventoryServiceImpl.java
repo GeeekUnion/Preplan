739e5ea5cd9781e88a5c792ece46aceceb429e67
b809220024@163.com <b809220024@163.com>
2017-06-03 22:29:24 +0800

    z

--- plan-module-service/src/main/java/com/gsafety/plan/service/impl/InventoryServiceImpl.java
+++ plan-module-service/src/main/java/com/gsafety/plan/service/impl/InventoryServiceImpl.java
@@ -10,6 +10,7 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import org.springframework.stereotype.Service;
+
 import com.gsafety.cloudframework.common.base.dao.IBaseDAO;
 import com.gsafety.cloudframework.common.base.page.PageResult;
 import com.gsafety.cloudframework.common.base.service.impl.BaseServiceImpl;
@@ -78,18 +79,44 @@ public class InventoryServiceImpl extends BaseServiceImpl implements InventorySe
 		//这里虽然是<100,但是结果是10公里之内. 应该是100*100
 		 String sql="select inventory_longitude as longitude,inventory_latitude as latitude,inventory_name as name,inventory_sn as sn from pre_inventory where sqrt(( ((117.147683-pre_inventory.inventory_longitude)*PI()*12656*cos(((34.220772+pre_inventory.inventory_latitude)/2)*PI()/180)/180)  *  ((117.147683-pre_inventory.inventory_longitude)*PI()*12656*cos (((34.220772+pre_inventory.inventory_latitude)/2)*PI()/180)/180)  )  +  (  ((34.220772-pre_inventory.inventory_latitude)*PI()*12656/180)  *  ((34.220772-pre_inventory.inventory_latitude)*PI()*12656/180)))<22"+
 				" UNION "+ 
-				"select hazard_longitude as longitude,hazard_latitude as latitude,hazard_name as name,hazard_sn as sn from pre_hazard where sqrt(( ((117.147683-hazard_longitude)*PI()*12656*cos(((34.220772+hazard_latitude)/2)*PI()/180)/180)  *  ((117.147683-hazard_longitude)*PI()*12656*cos (((34.220772+hazard_latitude)/2)*PI()/180)/180)  )  +  (  ((34.220772-hazard_latitude)*PI()*12656/180)  *  ((34.220772-hazard_latitude)*PI()*12656/180)))<22" ;
-		
+				"select hazard_longitude as longitude,hazard_latitude as latitude,hazard_name as name,hazard_sn as sn from pre_hazard where sqrt(( ((117.147683-hazard_longitude)*PI()*12656*cos(((34.220772+hazard_latitude)/2)*PI()/180)/180)  *  ((117.147683-hazard_longitude)*PI()*12656*cos (((34.220772+hazard_latitude)/2)*PI()/180)/180)  )  +  (  ((34.220772-hazard_latitude)*PI()*12656/180)  *  ((34.220772-hazard_latitude)*PI()*12656/180)))<22" +
+		        " UNION "+
+				"select emergencyResponseTeam_longitude as longitude,emergencyResponseTeam_latitude as latitude,emergencyResponseTeam_name as name,emergencyResponseTeam_sn as sn from pre_emergencyresponseteam where sqrt(( ((117.147683-emergencyResponseTeam_longitude)*PI()*12656*cos(((34.220772+emergencyResponseTeam_latitude)/2)*PI()/180)/180)  *  ((117.147683-emergencyResponseTeam_longitude)*PI()*12656*cos (((34.220772+emergencyResponseTeam_latitude)/2)*PI()/180)/180)  )  +  (  ((34.220772-emergencyResponseTeam_latitude)*PI()*12656/180)  *  ((34.220772-emergencyResponseTeam_latitude)*PI()*12656/180)))<22" +
+		        " UNION "+
+				"select protectionObject_longitude as longitude,protectionObject_latitude as latitude,protectionObject_name as name,protectionObject_sn as sn from pre_protectionobject where sqrt(( ((117.147683-protectionObject_longitude)*PI()*12656*cos(((34.220772+protectionObject_latitude)/2)*PI()/180)/180)  *  ((117.147683-protectionObject_longitude)*PI()*12656*cos (((34.220772+protectionObject_latitude)/2)*PI()/180)/180)  )  +  (  ((34.220772-protectionObject_latitude)*PI()*12656/180)  *  ((34.220772-protectionObject_latitude)*PI()*12656/180)))<22" ;
+
+
 		 // String sql="select inventory_longitude,inventory_latitude,inventory_name,inventory_sn from pre_inventory";
         ArrayList<Object[]> List =(ArrayList<Object[]>) baseDAO.getListBySql(sql);
         JSONArray array = new JSONArray();
 		for(int i=0;i<List.size();i++){
 			JSONObject jo = new JSONObject();
-			// jo.put("latitude", List.elementData.);
+			
 			jo.put("longitude", List.get(i)[0]);
 			jo.put("latitude", List.get(i)[1]);
 			jo.put("name", List.get(i)[2]);
 			jo.put("sn", List.get(i)[3]);
+		    //获取Sn里面的第一个char，以此来区分不同的资源点类型
+			   try {
+			   String sn=(String) List.get(i)[3];
+			    char fisrtChar=sn.charAt(0);
+				switch(fisrtChar){
+				case 'a':
+					jo.put("type","inventory" );
+					break;
+				case 'b':
+					jo.put("type","hazard" );
+					break;
+				case 'c':
+					jo.put("type","emergencyResponseTeam" );
+					break;
+				case 'd':
+					jo.put("type","protectionObject" );
+					break;	
+				}
+			} catch (Exception e) {
+				System.out.println("sn不存在或者sn的命名不规范，sn首字母不是a/b/c/d等");
+			}
 			array.add(jo);
 		}
 		 return array;
